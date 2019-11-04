865fb1b8b0da211d724b248dcb2aa655d649e874
Christopher Ruiz <chrisruizaz@gmail.com>
2019-11-03 20:23:12 -0700

    finished the db step

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Beef Egg Bacon Burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Veggie Burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('King Bacon Burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Munster Mash Burger');
